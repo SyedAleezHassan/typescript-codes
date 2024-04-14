25cb452aed3f2fba8ba886055d4b9cf8f8523e30
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-04-14 16:32:09 +0500

    Create prime_num.ts

---
+++ prime_num.ts
@@ -0,0 +1,23 @@
+function isPrime(num: number): boolean {
+    if (num <= 1) {
+        return false;
+    }
+    if (num <= 3) {
+        return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+    }
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
+
+// Example usage
+const numberToCheck = 17; // Change this number to check if it's prime
+console.log(`${numberToCheck} is prime? ${isPrime(numberToCheck)}`);
